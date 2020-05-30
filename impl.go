5a05af02bd81f021682074739f9bb128297ef263
Eduardo ES Riccardi <ed@kukino.uk>
2020-05-31 00:09:36 +0100

    Frodo Algorithm 1, encode, Kem Encapsulate wip

--- impl.go
+++ impl.go
@@ -2,7 +2,6 @@ package go_frodokem
 
 import (
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"fmt"
 )
@@ -50,13 +49,12 @@ func (k *FrodoKEM) Encapsulate(pk []uint8) (ct []uint8, ssEnc []uint8, err error
 	}
 	seedA := pk[0 : k.lenSeedA/8]
 	b := pk[k.lenSeedA/8:]
-	mu := make([]byte, k.lenMu/8)
+	mu := make([]uint8, k.lenMu/8)
 	k.rng(mu)
 	//fmt.Println("seedA", hex.EncodeToString(seedA))
 	//fmt.Println("b", hex.EncodeToString(b))
 	//fmt.Println("mu", hex.EncodeToString(mu))
-	pkh := k.shake(pk, k.lenPkh/8)
-	fmt.Println("pkh", hex.EncodeToString(pkh))
+	pkh := k.shake(pk, k.lenPkh/8) // fmt.Println("pkh", hex.EncodeToString(pkh))
 	seedSE_k := k.shake(append(pkh, mu...), k.lenSeedSE/8+k.lenK/8)
 	seedSE := seedSE_k[0 : k.lenSeedSE/8]
 	_k := seedSE_k[k.lenSeedSE/8 : k.lenSeedSE/8+k.lenK/8]
@@ -68,17 +66,16 @@ func (k *FrodoKEM) Encapsulate(pk []uint8) (ct []uint8, ssEnc []uint8, err error
 	Sprime := k.sampleMatrix(r[0:k.mBar*k.n], k.mBar, k.n)            // fmt.Println("S'", Sprime)
 	Eprime := k.sampleMatrix(r[k.mBar*k.n:2*k.mBar*k.n], k.mBar, k.n) // fmt.Println("E'", Eprime)
 	A := k.gen(seedA)
-	Bprime := matrixAdd(matrixMulWithMod2(Sprime, A, k.q), Eprime) // fmt.Println("B'", Bprime)
-	c1 := k.pack(Bprime)
-	fmt.Println("c1", hex.EncodeToString(c1))
-	Eprimeprime := k.sampleMatrix(r[2*k.mBar*k.n:2*k.mBar*k.n+k.mBar*k.nBar], k.mBar, k.nBar)
-	fmt.Println("E''", Eprimeprime)
+	Bprime := matrixAdd(matrixMulWithMod2(Sprime, A, k.q), Eprime)                            // fmt.Println("B'", Bprime)
+	c1 := k.pack(Bprime)                                                                      // fmt.Println("c1", hex.EncodeToString(c1))
+	Eprimeprime := k.sampleMatrix(r[2*k.mBar*k.n:2*k.mBar*k.n+k.mBar*k.nBar], k.mBar, k.nBar) // fmt.Println("E''", Eprimeprime)
 	B := k.unpack(b, k.n, k.nBar)
-
 	V := matrixAdd(matrixMulWithMod2(Sprime, B, k.q), Eprimeprime)
+	muEncoded := k.encode(mu)
+
 	//C := matrixAdd(V, unpackUint16(mu))
 
-	fmt.Println(len(_k), len(c1), len(Eprimeprime), len(B), len(V))
+	fmt.Println(len(_k), len(c1), len(Eprimeprime), len(B), len(V), len(muEncoded))
 
 	return
 }
@@ -198,7 +195,7 @@ func (k *FrodoKEM) sampleMatrix(r []uint16, n1 int, n2 int) (E [][]int16) {
 	return E
 }
 
-// Algorithm 3: Frodo.Pack
+// FrodoKEM specification, Algorithm 3: Frodo.Pack
 func (k *FrodoKEM) pack(C [][]uint16) (r []byte) {
 	rows := len(C)
 	cols := len(C[0])
@@ -210,7 +207,7 @@ func (k *FrodoKEM) pack(C [][]uint16) (r []byte) {
 		for j := 0; j < cols; j++ {
 			val := C[i][j]
 			for b := 0; b < k.D; b++ {
-				bit := bitn(val, k.D-b-1)
+				bit := uint16BitN(val, k.D-b-1)
 				packed <<= 1
 				packed |= bit
 				bits++
@@ -229,13 +226,69 @@ func (k *FrodoKEM) pack(C [][]uint16) (r []byte) {
 	return r
 }
 
+// FrodoKEM specification, Algorithm 4: Frodo.Unpack
 func (k *FrodoKEM) unpack(b []uint8, n1 int, n2 int) (C [][]uint16) {
-	// TODO
+	C = make([][]uint16, n1)
+	for i := 0; i < n1; i++ {
+		C[i] = make([]uint16, n2)
+	}
+	bIdx := 0
+	BBit := 7
+	for i := 0; i < n1; i++ {
+		for j := 0; j < n2; j++ {
+			for l := 0; l < k.D; l++ {
+				bit := uint8BitN(b[bIdx], BBit)
+				BBit--
+				if BBit < 0 {
+					BBit = 7
+					bIdx++
+				}
+				C[i][j] <<= 1
+				if bit > 0 {
+					C[i][j] |= 1
+				}
+			}
+		}
+	}
 	return
 }
 
-func bitn(val uint16, i int) uint8 {
+// FrodoKEM specification, Algorithm 1
+func (k *FrodoKEM) encode(b []uint8) (K [][]uint16) {
+	K = make([][]uint16, k.mBar)
+	for i := 0; i < k.mBar; i++ {
+		K[i] = make([]uint16, k.nBar)
+	}
+	multiplier := k.q
+	if k.B > 0 {
+		multiplier /= 2 << (k.B - 1)
+	}
+	bIdx := 0
+	BBit := 0
+	for i := 0; i < k.mBar; i++ {
+		for j := 0; j < k.nBar; j++ {
+			for l := 0; l < k.B; l++ {
+				bit := uint8BitN(b[bIdx], BBit)
+				fmt.Print(bit)
+				if BBit++; BBit > 7 {
+					BBit = 0
+					bIdx++
+				}
+				if bit > 0 {
+					K[i][j] |= 1 << l
+				}
+			}
+			K[i][j] *= multiplier
+		}
+	}
+	return
+}
+
+func uint16BitN(val uint16, i int) uint8 {
 	return uint8((val >> i) & 1)
+}
+func uint8BitN(val uint8, i int) uint8 {
+	return (val >> i) & 1
 }
 
 func (k *FrodoKEM) OverrideRng(newRng func([]byte)) {
