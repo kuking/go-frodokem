3af787b97c6a86b8b4d4fe51d54c32a4936683d5
Eduardo ES Riccardi <ed@kukino.uk>
2020-05-31 01:56:22 +0100

    minor cleanup in benchmarks output

--- bench_test.go
+++ bench_test.go
@@ -6,11 +6,11 @@ import (
 
 func suites() map[string]FrodoKEM {
 	return map[string]FrodoKEM{
-		" 640 AES  ": Frodo640AES(),
+		"   640 AES": Frodo640AES(),
+		"   976 AES": Frodo976AES(),
+		"  1344 AES": Frodo1344AES(),
 		" 640 SHAKE": Frodo640SHAKE(),
-		" 976 AES  ": Frodo976AES(),
 		" 976 SHAKE": Frodo976SHAKE(),
-		"1344 AES  ": Frodo1344AES(),
 		"1344 SHAKE": Frodo1344SHAKE(),
 	}
 }
@@ -19,7 +19,7 @@ func Benchmark_FrodoKEM_Keygen(b *testing.B) {
 	for name, kem := range suites() {
 		b.Run(name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				kem.Keygen()
+				_, _ = kem.Keygen()
 			}
 		})
 	}
@@ -30,7 +30,7 @@ func Benchmark_FrodoKEM_Encapsulate(b *testing.B) {
 		b.Run(name, func(b *testing.B) {
 			pk, _ := kem.Keygen()
 			for i := 0; i < b.N; i++ {
-				kem.Encapsulate(pk)
+				_, _, _ = kem.Encapsulate(pk)
 			}
 		})
 	}
