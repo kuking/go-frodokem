626df49bb6cd9266ec01418c4b51adc9c72f44e8
Eduardo ES Riccardi <ed@kukino.uk>
2020-12-08 20:01:08 +0000

    improved wording

--- agreement_test.go
+++ agreement_test.go
@@ -9,37 +9,34 @@ import (
 func TestKeyAgreementExample(t *testing.T) {
 	for _, kem := range Variants() {
 
-		// Stage 1, both Bob and Alice generates their long-lived key-pair, this is once in a while event.
+		// Stage 0, both Bob and Alice generates their long-lived key-pair, this is once in a while event.
 		// We assume each other party receives the counter party public key in a secure way.
 		//(To prevent a Man-In-The-Middle attack.)
-
 		AlicePublic, AliceSecret := kem.Keygen()
 		BobPublic, BobSecret := kem.Keygen()
-		// After this stage, we assume Bob has Alice' public key, and Alice has Bob' public key
 
-		// Alice generates a random number holding it in AliceShare and a cipherText for Bob.
-		// ditto Bob for Alice
+		// Stage 1, Agreeing on temporary Key to be used in a symmetric cipher using an insecure channel
+		// Alice generates a random number holding it in AliceShare and a cipherText for Bob (& ditto Bob for Alice).
 		AliceToBobCipherText, AliceShare, err := kem.Encapsulate(BobPublic)
 		BobToAliceCipherText, BobShare, err := kem.Encapsulate(AlicePublic)
 
-		// Alice sends to Bob 'AliceToBobCipherText' over an insecure channel
-		// ditto Bob 'BobToAliceCipherText'
+		// Stage 2, both cipher texts are exchanged via an insecure channel.
+		// Alice sends to Bob 'AliceToBobCipherText' over an insecure wire (& ditto Bob 'BobToAliceCipherText')
 
-		// Alice has BobToAliceCipherText and can reconstruct BobShare using Alice Private key.
+		// Stage 3, Alice has BobToAliceCipherText and can reconstruct BobShare using Alice Private key.
 		AliceCopyOfBobShare, err := kem.Dencapsulate(AliceSecret, BobToAliceCipherText)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatal(err) // if the cipherText was corrupted or compromised, Dencapsulate might err.
 		}
-
 		// Ditto  Bob
 		BobCopyOfAliceShare, err := kem.Dencapsulate(BobSecret, AliceToBobCipherText)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatal(err) // ditto, might fail if compromised or corrupted ciphertext.
 		}
 
 		// In practice, the following assertion can not be verified, as AliceCopyOfBobShare and BobShare will be in
 		// different computers. They should be equal. The real-life verification happens when the final derived key
-		// from each party become the same value, enabling the symmetric cipher to work. Below.
+		// from each party become the same value, enabling the symmetric cipher to work correctly. Below in stage 4.
 		if !bytes.Equal(AliceCopyOfBobShare, BobShare) {
 			t.Fatal("Alice failed to reconstruct Bob Shared secret")
 		}
@@ -48,9 +45,10 @@ func TestKeyAgreementExample(t *testing.T) {
 			t.Fatal("Bob failed to reconstruct Alice Shared secret")
 		}
 
-		// Key exchange has occurred, now both parties can generate a common shared secret that is built using both
-		// parties shared secret, the share was nos transfered via the wire, but the cipher text which is considered
-		// secure. The final joined key can be used for a session key (i.e. AES)
+		// Stage 4, Key exchange has occurred, now both parties can generate a common shared secret that is built using
+		// both parties shared secret, the shared secret was not transfer in plain-text over the wire, but derived from
+		// the ciphertexts. If modified in any way or form, the final key won't be the same for both parties.
+		// The final joined key can be used for a session key in a symmetric cipher (i.e. AES)
 		AliceCopyOfSharedKey := append(AliceShare, AliceCopyOfBobShare...)
 		BobCopyOfSharedKey := append(BobCopyOfAliceShare, BobShare...)
 		if !bytes.Equal(AliceCopyOfSharedKey, BobCopyOfSharedKey) {
